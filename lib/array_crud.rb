692d4990196aa61d2f6de3cb6ebfd9e14a7d7704
Lauren Dove <elledoveinc@gmail.com>
2019-08-11 03:26:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ end
 def retrieve_element_from_index(array, index_number)
   array[index_number]
 
-end #still needs to be solved!
+end #solved!
 
 def retrieve_first_element_from_array(array)
  array[0] #this one is correct! so dont change it Lauren
